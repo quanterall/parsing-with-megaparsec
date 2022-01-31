0af6decb50b97e81e52ee8ec47419f9454a082e7
Rickard Andersson <gonz@severnatazvezda.com>
2022-01-31 20:45:24 +0200

    add string interpolation & tests
    Also more value assignment tests

--- src/ScriptingLanguage.hs
+++ src/ScriptingLanguage.hs
@@ -2,14 +2,20 @@ module ScriptingLanguage where
 
 import HostsParser (Filename (..))
 import RIO
+import qualified RIO.Map as Map
+import qualified RIO.Set as Set
+import qualified RIO.Text as Text
 import qualified Text.Megaparsec as Megaparsec
 import qualified Text.Megaparsec.Char as MChar
 import qualified Text.Megaparsec.Char.Lexer as Lexer
 
 type Parser = Megaparsec.ParsecT Void Text (RIO ParsingState)
 
+newtype BindingName = BindingName {unBindingName :: Text}
+  deriving (Eq, Ord, Show)
+
 data ParsingState = ParsingState
-  { bindingsRef :: !(IORef (Map Text Expression))
+  { bindingsRef :: !(IORef (Map BindingName Expression))
   }
 
 data ScriptComponent
@@ -18,7 +24,7 @@ data ScriptComponent
   deriving (Eq, Show)
 
 data Statement
-  = AssignValue !Text !Expression
+  = AssignValue !BindingName !Expression
   | IfStatement !Expression ![ScriptComponent] ![ScriptComponent]
   deriving (Eq, Show)
 
@@ -27,7 +33,13 @@ data Expression
   | IntegerLiteral !Integer
   | FloatLiteral !Double
   | BooleanLiteral !Bool
-  | ShellCommand !(Maybe ShellCommandComponent)
+  | InterpolatedString ![StringInterpolationFragment]
+  | ShellCommand !Text !(Maybe ShellCommandComponent)
+  deriving (Eq, Show)
+
+data StringInterpolationFragment
+  = LiteralFragment !Text
+  | BindingFragment !BindingName
   deriving (Eq, Show)
 
 data ShellCommandComponent
@@ -48,16 +60,137 @@ parseScript (Filename filename) text = do
   runRIO initialState $ Megaparsec.runParserT scriptP filename text
 
 scriptP :: Parser [ScriptComponent]
-scriptP = undefined
+scriptP =
+  Megaparsec.sepBy
+    (Megaparsec.choice [Statement <$> statementP, Expression <$> expressionP])
+    (some MChar.newline)
+
+statementP :: Parser Statement
+statementP = do
+  Megaparsec.choice [assignValueP, ifStatementP]
+
+assignValueP :: Parser Statement
+assignValueP = do
+  bindingName <- lexeme bindingNameP
+  _ <- symbol "="
+  expression <- expressionP
+  bindValue bindingName expression
+  pure $ AssignValue bindingName expression
+
+bindValue :: BindingName -> Expression -> Parser ()
+bindValue bindingName expression = do
+  ref <- asks bindingsRef
+  modifyIORef ref $ Map.insert bindingName expression
+
+bindingNameP :: Parser BindingName
+bindingNameP = do
+  initialCharacter <- MChar.letterChar
+  restOfName <- Megaparsec.many (MChar.alphaNumChar <|> MChar.char '_')
+  pure $ BindingName $ Text.pack (initialCharacter : restOfName)
+
+ifStatementP :: Parser Statement
+ifStatementP = undefined
+
+expressionP :: Parser Expression
+expressionP =
+  Megaparsec.choice
+    [ stringLiteralP,
+      integerLiteralP,
+      floatLiteralP,
+      booleanLiteralP,
+      interpolatedStringP,
+      shellCommandP
+    ]
+
+stringLiteralP :: Parser Expression
+stringLiteralP = do
+  _ <- MChar.char '\"'
+  string <-
+    Megaparsec.manyTill (Megaparsec.try readEscapedQuote <|> MChar.printChar) (MChar.char '\"')
+  pure $ StringLiteral $ Text.pack string
+  where
+    readEscapedQuote :: Parser Char
+    readEscapedQuote = do
+      MChar.char '\\' *> MChar.char '\"'
+
+interpolatedStringP :: Parser Expression
+interpolatedStringP = do
+  _ <- MChar.char '`'
+  fragments <- Megaparsec.manyTill interpolationFragmentP (MChar.char '`')
+  pure $ InterpolatedString fragments
+
+interpolationFragmentP :: Parser StringInterpolationFragment
+interpolationFragmentP =
+  Megaparsec.choice [Megaparsec.try bindingFragmentP, literalFragmentP]
+
+bindingFragmentP :: Parser StringInterpolationFragment
+bindingFragmentP = do
+  _ <- MChar.char '{'
+  bindingName <- bindingNameP
+  _ <- MChar.char '}'
+  checkBoundVariable bindingName
+  pure $ BindingFragment bindingName
+
+checkBoundVariable :: BindingName -> Parser ()
+checkBoundVariable bindingName = do
+  reference <- asks bindingsRef
+  bindings <- readIORef reference
+  case Map.lookup bindingName bindings of
+    Nothing -> reportError $ "Variable " <> show bindingName <> " is unbound"
+    Just _ -> pure ()
+
+reportError :: String -> Parser a
+reportError = Megaparsec.ErrorFail >>> Set.singleton >>> Megaparsec.fancyFailure
+
+literalFragmentP :: Parser StringInterpolationFragment
+literalFragmentP = (Text.pack >>> LiteralFragment) <$> Megaparsec.some literalFragmentCharacterP
+  where
+    literalFragmentCharacterP :: Parser Char
+    literalFragmentCharacterP = Megaparsec.satisfy (`notElem` ['`', '{'])
+
+integerLiteralP :: Parser Expression
+integerLiteralP = IntegerLiteral <$> Lexer.signed (pure ()) Lexer.decimal
+
+floatLiteralP :: Parser Expression
+floatLiteralP = FloatLiteral <$> Lexer.signed (pure ()) Lexer.float
+
+booleanLiteralP :: Parser Expression
+booleanLiteralP = do
+  text <- ["true", "false"] & fmap MChar.string & Megaparsec.choice
+  pure $ BooleanLiteral $ text == "true"
+
+shellCommandP :: Parser Expression
+shellCommandP = do
+  _ <- MChar.char '\''
+  command <-
+    Text.pack
+      <$> Megaparsec.manyTill
+        (Megaparsec.try readEscapedSingleQuote <|> MChar.printChar)
+        (MChar.char '\'')
+  maybeShellCommandComponent <- Megaparsec.optional shellCommandComponentP
+  pure $ ShellCommand command maybeShellCommandComponent
+  where
+    readEscapedSingleQuote :: Parser Char
+    readEscapedSingleQuote = do
+      MChar.char '\\' *> MChar.char '\''
+
+    shellCommandComponentP :: Parser ShellCommandComponent
+    shellCommandComponentP = do
+      _ <- MChar.char '.'
+      Megaparsec.choice
+        [ MChar.string "out" *> pure ShellStandardOut,
+          MChar.string "err" *> pure ShellStandardError,
+          MChar.string "code" *> pure ShellExitCode
+        ]
 
 -- | Defines how whitespace is consumed.
 spaceConsumer :: Parser ()
-spaceConsumer = Lexer.space MChar.space1 (Lexer.skipLineComment "# ") Megaparsec.empty
+spaceConsumer = Lexer.space MChar.space1 (Lexer.skipLineComment "// ") Megaparsec.empty
 
--- | Reads any amount of whitespace and then applies a parser.
+-- | Applies a parser and any amount of whitespace after.
 lexeme :: Parser a -> Parser a
 lexeme = Lexer.lexeme spaceConsumer
 
--- | Reads any amount of whitespace and then a specific string of text.
+-- | Reads a specific string of text and any amount of whitespace after.
 symbol :: Text -> Parser Text
 symbol = Lexer.symbol spaceConsumer
--- test/ScriptingLanguageSpec.hs
+++ test/ScriptingLanguageSpec.hs
@@ -2,9 +2,9 @@ module ScriptingLanguageSpec where
 
 import HostsParser (Filename (..))
 import RIO
-import qualified RIO.Text as Text
 import ScriptingLanguage
 import Test.Hspec
+import Text.Megaparsec.Error
 
 testFile :: FilePath
 testFile = "test-data/test.glue"
@@ -12,8 +12,109 @@ testFile = "test-data/test.glue"
 spec :: Spec
 spec = do
   describe "Parsing" $ do
-    describe "Basic string assignment" $
+    describe "Basic string assignment" $ do
       it "should create an instruction to store a string into a binding" $ do
         let text = "value = \"test\""
-            result = parseScript (Filename "test.glue") text
-        result `shouldReturn` Right [Statement $ AssignValue "value" (StringLiteral "test")]
+        result <- parseScript (Filename "test.glue") text
+        expectRight result
+        result
+          `shouldBe` Right
+            [Statement $ AssignValue (BindingName "value") (StringLiteral "test")]
+
+      it "should handle escaped quotes in string values" $ do
+        let text = "value = \"test\\\" value\""
+        result <- parseScript (Filename "test.glue") text
+        expectRight result
+        result
+          `shouldBe` Right
+            [Statement $ AssignValue (BindingName "value") (StringLiteral "test\" value")]
+
+      it "should be able to read two assignments in a row" $ do
+        let text = "value1 = \"test1\"\nvalue2 = \"test2\""
+        result <- parseScript (Filename "test.glue") text
+        result
+          `shouldBe` Right
+            [ Statement $ AssignValue (BindingName "value1") (StringLiteral "test1"),
+              Statement $ AssignValue (BindingName "value2") (StringLiteral "test2")
+            ]
+
+    describe "String interpolation" $ do
+      it "should be able to interpolate a string without any binding fragments" $ do
+        let text = "value = `test`"
+        result <- parseScript (Filename "test.glue") text
+        expectRight result
+        result
+          `shouldBe` Right
+            [ Statement $
+                AssignValue
+                  (BindingName "value")
+                  (InterpolatedString [LiteralFragment "test"])
+            ]
+
+      it "should be able to interpolate a string with a binding fragment" $ do
+        let text = "test = \"hello\"\nvalue = `test {test}`"
+        result <- parseScript (Filename "test.glue") text
+        expectRight result
+        result
+          `shouldBe` Right
+            [ Statement $
+                AssignValue
+                  (BindingName "test")
+                  (StringLiteral "hello"),
+              Statement $
+                AssignValue
+                  (BindingName "value")
+                  ( InterpolatedString
+                      [ LiteralFragment "test ",
+                        BindingFragment (BindingName "test")
+                      ]
+                  )
+            ]
+
+      it "should be able to interpolate a string both before and after a binding fragment" $ do
+        let text = "test = \"hello\"\nvalue = `test {test} after as well`"
+        result <- parseScript (Filename "test.glue") text
+        expectRight result
+        result
+          `shouldBe` Right
+            [ Statement $
+                AssignValue
+                  (BindingName "test")
+                  (StringLiteral "hello"),
+              Statement $
+                AssignValue
+                  (BindingName "value")
+                  ( InterpolatedString
+                      [ LiteralFragment "test ",
+                        BindingFragment (BindingName "test"),
+                        LiteralFragment " after as well"
+                      ]
+                  )
+            ]
+
+      it "should be able to interpolate a string, binding, string, binding and string" $ do
+        let text = "test = \"hello\"\nvalue = `test {test} after {test} as well`"
+        result <- parseScript (Filename "test.glue") text
+        expectRight result
+        result
+          `shouldBe` Right
+            [ Statement $
+                AssignValue
+                  (BindingName "test")
+                  (StringLiteral "hello"),
+              Statement $
+                AssignValue
+                  (BindingName "value")
+                  ( InterpolatedString
+                      [ LiteralFragment "test ",
+                        BindingFragment (BindingName "test"),
+                        LiteralFragment " after ",
+                        BindingFragment (BindingName "test"),
+                        LiteralFragment " as well"
+                      ]
+                  )
+            ]
+
+expectRight :: Either (ParseErrorBundle Text Void) a -> Expectation
+expectRight (Right _) = return ()
+expectRight (Left e) = expectationFailure $ errorBundlePretty e
